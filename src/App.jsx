67c779772b312caca8f56e4f715584e2acdca111
Hurtadx <andreshm253@gmail.com>
2025-02-07 13:35:55 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lavaca PROYECTO!
+
+      </h1>
+      <main>
+        {
+          
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
