48bdb6db809f8f42e4af70706a95c8e0b5fa756b
Hurtadx <andreshm253@gmail.com>
2025-02-13 07:36:28 -0500

    Update styles for Header and Footer components; add logo to Header and enhance App background

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './Header.css';
+import logo from '../../App/Img/LogoLaVaca.png';
 
 const Header = () => {
     return (
         <header>
         <div className="header-container">
         <div className="title">
-            <img></img>
+            <img src={logo}></img>
             <h1>LaVaca</h1>
         </div>
         </div>
