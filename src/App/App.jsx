8028e0029d586ddf7762ee860c5915856fccd78c
Hurtadx <andreshm253@gmail.com>
2025-02-13 16:03:16 -0500

    Add animated Savings icon and update layout; enhance App structure and styles for Footer and AuthForm

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,41 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import AuthForm from '../assets/AuthForm/AuthForm'
+import { useState } from 'react';
+import './App.css';
+import AuthForm from '../assets/AuthForm/AuthForm';
 import Header from '../Components/header/Header';
 import Footer from '../Components/Footer/Footer';
+import SavingsIcon from '../assets/svg/LoginIcon';
 
 function App() {
   const [authType, setAuthType] = useState('login');
 
-
-const handleTypeChange = (type) => {
-  setAuthType(prevType => prevType === 'login' ? 'register' : 'login');
-}
+  const handleTypeChange = (type) => {
+    setAuthType(prevType => prevType === 'login' ? 'register' : 'login');
+  }
 
   return (
     <div className="App">
       <Header />
 
-      <div className="auth-container">
-        <div className="auth-buttons">
-        
-        <AuthForm 
-          type={authType} 
-          onTypeChange={handleTypeChange}
-          />
-
-       
+      <div className="main-content">
+        <SavingsIcon/>
+        <div className="auth-container">
+          <div className="auth-buttons">
+            <AuthForm 
+              type={authType} 
+              onTypeChange={handleTypeChange}
+            />
+          </div>
         </div>
       </div>
 
-
-    <div className='footer-container'>
-      <Footer/>
+      <div className='footer-container'>
+        <Footer />
+      </div>
     </div>
-      
-
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
