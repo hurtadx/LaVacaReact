dca2e96d1901504eb4458a0f5c1422b7bfcf580c
Hurtadx <andreshm253@gmail.com>
2025-02-19 14:09:27 -0500

    Refactor CSS variables in index.css for improved maintainability; streamline styling for links and buttons

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { NotificationProvider } from './assets/Notification/NotificationContext';
+import App from './App/App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
+  </StrictMode>
+);
