84a69d8314a813330347c83822a474a3d12c6493
Hurtadx <andreshm253@gmail.com>
2025-02-27 14:11:31 -0500

    Refactor CSS styles: consolidate color variables into a new variables.css file, update class names for consistency, and enhance layout responsiveness across components

--- src/Layout/header/Header.jsx
+++ src/Layout/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="site-header" role="banner">
       <div className="header-container">
-        <div className="title">
+        <div className="header-title">
           <img src={logo} alt="LaVaca Banking Logo" />
           <h1>LaVaca</h1>
         </div>
