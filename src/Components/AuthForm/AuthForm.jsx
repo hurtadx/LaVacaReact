18329dd8f47d5d1453a6611aa5f01ee74362cee9
Hurtadx <andreshm253@gmail.com>
2025-03-18 16:23:50 -0500

    Refactor AuthForm: Integrate AnimatedCarrousel and enhance layout for improved user experience

--- src/Components/AuthForm/AuthForm.jsx
+++ src/Components/AuthForm/AuthForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LoginForm from "./loginForm";
 import RegisterForm from "./registerForm";
 import "./AuthForm.css";
+import AnimatedCarrousel from "../AnimatedCarrousel/AnimatedCarrousel";
 
 const AuthForm = ({ type: initialType }) => {
   const [type, setType] = useState(initialType || "login");
@@ -11,12 +12,28 @@ const AuthForm = ({ type: initialType }) => {
   };
   
   return (
-    <div className="auth-container">
-      {type === "login" ? (
-        <LoginForm onSwitchToRegister={toggleFormType} />
-      ) : (
-        <RegisterForm onSwitchToLogin={toggleFormType} />
-      )}
+    <div className="auth-page-container">
+      {/* Panel izquierdo con imagen */}
+      <div className="auth-left-panel">
+        <div className="decorative-line"></div>
+        <div className="decorative-line"></div>
+        <div className="auth-image-container">
+          <AnimatedCarrousel/>
+        </div>
+        <div className="auth-welcome-text">
+          <h1>¡Bienvenido a La Vaca!</h1>
+          <p>La plataforma para organizar colectas y ahorros grupales de manera sencilla y segura.</p>
+        </div>
+      </div>
+      
+      {/* Contenedor del formulario a la derecha */}
+      <div className="auth-container">
+        {type === "login" ? (
+          <LoginForm onSwitchToRegister={toggleFormType} />
+        ) : (
+          <RegisterForm onSwitchToLogin={toggleFormType} />
+        )}
+      </div>
     </div>
   );
 };
